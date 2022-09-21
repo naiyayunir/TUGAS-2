09d6062941410a675d4cba6bf12f943f140c284a
naiyayunir <ayunirnaiya@gmail.com>
2022-09-21 21:38:09 +0700

    edit

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -7,9 +7,8 @@ from mywatchlist.views import show_json_by_id
 app_name = 'mywatchlist'
 
 urlpatterns = [
-    path('', show_mywatchlist, name='show_movie'),
+    path('html/', show_mywatchlist, name='show_movie'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
     path('json/<int:id>', show_json_by_id, name='show_json_by_id')
-
 ]
